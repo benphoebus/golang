06ce160ad129fdb5ed337e193f1d9a778c16b588
benphoebus <46415852+benphoebus@users.noreply.github.com>
2022-09-26 14:38:11 +1000

    Add files via upload

---
+++ go-crud-api/main.go
@@ -0,0 +1,103 @@
+package main
+
+import(
+	"fmt"
+	"log"
+	"net/http"
+	"encoding/json"
+	"math/rand"
+	"strconv"
+	"github.com/gorilla/mux"
+)
+type Movie struct {
+	ID string `json:"id:`
+	Isbn string `json:"isbn"`
+	Title string `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct{
+	FirstName string `json:"firstname"`
+	LastName string `json:lastname"`
+}
+
+var movies []Movie
+
+func getMovies(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(movies)
+}
+
+func deleteMovie(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range movies{
+
+		if item.ID == params["id"]{
+			// delete movie at index, and append all movies after index to movies array
+			movies = append(movies[:index], movies[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(movies)
+}
+
+func getMovie(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range movies{
+		if item.ID == params["id"]{
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+}
+
+func createMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var movie Movie
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	movie.ID = strconv.Itoa(rand.Intn(100000000))
+	movies = append(movies, movie)
+	json.NewEncoder(w).Encode(movie)
+}
+
+func updateMovie(w http.ResponseWriter, r *http.Request){
+	// set json content type
+	w.Header().Set("Content-Type", "application/json")
+	// params
+	params := mux.Vars(r)
+	// loop over the movies, range
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			// delete the movie with the id that youve sent
+			movies = append(movies[:index], movies[index+1:]...)
+			// add a new movie - the movie that we send in the body
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = params["id"]
+			movies = append(movies, movie)
+			json.NewEncoder(w).Encode(movie)
+			return
+		}
+	}
+}
+
+func main() {
+	r := mux.NewRouter()
+	
+	// dummy movie data
+	movies = append(movies, Movie{ID: "1", Isbn: "438227", Title: "Movie1", Director : &Director{FirstName: "B", LastName: "P"}})
+	movies = append(movies, Movie{ID: "2", Isbn: "437527", Title: "Movie2", Director : &Director{FirstName: "Jane", LastName: "Smith"}})
+	movies = append(movies, Movie{ID: "3", Isbn: "438127", Title: "Movie3", Director : &Director{FirstName: "Alice", LastName: "Bob"}})
+
+	// api routes
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies/", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Println("Starting PORT at PORT:8080\n")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
